83214feb385e7ba9c52505ef853b677f65ab06dd
craciunelenaotilia <craciunotiliaelena@gmail.com>
2023-12-14 19:46:26 +0200

    modificari de la Otilia

--- Storeapp/urls.py
+++ Storeapp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('', storeapp, name='home'),
+    path('', views.storeapp, name='home'),
     # Add more patterns as needed
 ]
