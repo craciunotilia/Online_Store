214ac1e65fe114da8e5a4017b0a3cc80c6657f3a
petricavirlan <gpm_virlan@yahoo.com>
2023-12-15 21:05:45 +0200

    Petrica commit

--- Storeapp/models.py
+++ Storeapp/models.py
@@ -3,12 +3,22 @@ from django.db import models
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
+
+# class CardData(models.Model):
+#
+#  card_number =
+#     card_owener=
+#     cvv_code =
+#     year_expiration =
+#     month_expiration  =
+
 
 class ShoppingCartItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
