8afd891afcf5708edaf782be14ada5f24a2262e7
petricavirlan <gpm_virlan@yahoo.com>
2023-12-20 21:35:21 +0200

    urls fixes

--- Storeapp/urls.py
+++ Storeapp/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth import views as auth_views
 from .views import blog_view
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import shop_view
+from .views import checkout_view, order_confirmation_view
 
 urlpatterns = [
     path('', home, name='home'),
@@ -17,8 +18,10 @@ urlpatterns = [
     path('card_form/', views.card_data_view, name='card_form'),
     path('about/', about_view, name='about_view'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html', next_page='home'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout', next_page='home'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html', next_page='home'), name='logout'),
     path('blog/', blog_view, name='blog_view'),
     path('shop/', shop_view, name='shop_view'),
-
+    path('checkout/', checkout_view, name='checkout_view'),
+    path('order_confirmation/', order_confirmation_view, name='order_confirmation'),
+    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
 ]
